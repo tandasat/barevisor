4749b9c2d095261ced17600aa93bb8811db69be4
Satoshi Tanda <tanda.sat@gmail.com>
2024-04-27 09:04:57 -0700

    hvcore: clean up `vmx.rs`

--- src/hvcore/src/hypervisor/intel/vmx.rs
+++ src/hvcore/src/hypervisor/intel/vmx.rs
@@ -8,44 +8,21 @@ use crate::hypervisor::{
     x86_instructions::{cr0, cr0_write, cr4, cr4_write, rdmsr, wrmsr},
 };
 
+#[derive(Default)]
 pub(crate) struct Vmx {
     vmxon_region: Vmxon,
-    enabled: bool,
 }
 
 impl Extension for Vmx {
     fn enable(&mut self) {
-        assert!(!self.enabled);
         cr0_write(get_adjusted_cr0(cr0()));
         cr4_write(get_adjusted_cr4(cr4()));
         Self::adjust_feature_control_msr();
         vmxon(&mut self.vmxon_region);
-        self.enabled = true;
-    }
-}
-
-impl Default for Vmx {
-    fn default() -> Self {
-        Self::new()
-    }
-}
-
-impl Drop for Vmx {
-    fn drop(&mut self) {
-        if self.enabled {
-            vmxoff();
-        }
     }
 }
 
 impl Vmx {
-    pub(crate) fn new() -> Self {
-        Self {
-            vmxon_region: Vmxon::new(),
-            enabled: false,
-        }
-    }
-
     /// Updates an MSR to satisfy the requirement for entering VMX operation.
     fn adjust_feature_control_msr() {
         const IA32_FEATURE_CONTROL_LOCK_BIT_FLAG: u64 = 1 << 0;
@@ -67,30 +44,28 @@ impl Vmx {
     }
 }
 
-#[derive(Default, derive_deref::Deref, derive_deref::DerefMut)]
+#[derive(derive_deref::Deref, derive_deref::DerefMut)]
 struct Vmxon {
     ptr: Box<VmxonRaw>,
 }
 
-impl Vmxon {
-    fn new() -> Self {
+impl Default for Vmxon {
+    fn default() -> Self {
         let mut vmxon = zeroed_box::<VmxonRaw>();
         vmxon.revision_id = rdmsr(x86::msr::IA32_VMX_BASIC) as _;
         Self { ptr: vmxon }
     }
 }
 
-/// The region of memory that the logical processor uses to support VMX
-/// operation.
+/// The region of memory that the logical processor uses to support VMX operation.
 ///
 /// See: 25.11.5 VMXON Region
 #[derive(derivative::Derivative)]
-#[derivative(Debug, Default)]
+#[derivative(Debug)]
 #[repr(C, align(4096))]
 struct VmxonRaw {
     revision_id: u32,
     #[derivative(Debug = "ignore")]
-    #[derivative(Default(value = "[0; 4092]"))]
     data: [u8; 4092],
 }
 
@@ -101,10 +76,4 @@ fn vmxon(vmxon_region: &mut VmxonRaw) {
 
     // Safety: this project runs at CPL0.
     unsafe { x86::bits64::vmx::vmxon(pa).unwrap() };
-}
-
-/// The wrapper of the VMXOFF instruction.
-fn vmxoff() {
-    // Safety: this project runs at CPL0.
-    unsafe { x86::bits64::vmx::vmxoff().unwrap() };
 }
