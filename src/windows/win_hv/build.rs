a2a22cff328e39b056d35f428f25f14ebabf0d86
Satoshi Tanda <tanda.sat@gmail.com>
2024-11-17 10:10:30 -0800

    Revert back to use wdk-sys 0.2

--- src/windows/win_hv/build.rs
+++ src/windows/win_hv/build.rs
@@ -2,6 +2,7 @@
 
 fn main() -> Result<(), wdk_build::ConfigError> {
     let mut config = wdk_build::Config::from_env_auto()?;
-    config.driver_config = wdk_build::DriverConfig::Wdm;
-    config.configure_binary_build()
+    config.driver_config = wdk_build::DriverConfig::WDM();
+    config.configure_binary_build();
+    Ok(())
 }
